63dd5509f33301ada1121337d7235e0e8fdf1530
HUSEYIN6134 <huseyin.murat@st.uskudar.edu.tr>
2023-02-24 17:28:43 +0300

    InterfaceDemo
    Engin Demirog Java Kursu

---
+++ src/Worker.java
@@ -0,0 +1,22 @@
+//bizim çalışanımız
+public class Worker implements IWorkable,IEatable,IPayable {
+
+	@Override
+	public void work() {
+		
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Pay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
